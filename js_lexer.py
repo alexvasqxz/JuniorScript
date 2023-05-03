fbdc8a12689c2d0b4aefa1ce30018c372516b7e8
Gustavo Vasquez <gustavovasquez99@icloud.com>
2023-05-02 19:13:10 -0600

    JuniorScript Lexer with Tests

---
+++ js_lexer.py
@@ -0,0 +1,154 @@
+# ------------------------------------------------------------
+# juniorScript_parser.py
+#
+# (Python Lex & Yacc)
+# Analisis Lexico
+#
+# Gustavo Vasquez
+# ------------------------------------------------------------
+
+import ply.lex as lex
+
+palabras_reservadas = {
+    'mi_programa': 'PROGRAMA',
+    'MI_PROGRAMA': 'PROGRAMA',
+    'mi_funcion': 'FUNCION',
+    'MI_FUNCION': 'FUNCION',
+    'iniciar' : 'MAIN',
+    'INICIAR' : 'MAIN',
+    'variable': 'VAR',
+    'VARIABLE': 'VAR',
+    'entero': 'TIPOENT',
+    'ENTERO' : 'TIPOENT',
+    'decimal': 'TIPODEC',
+    'DECIMAL': 'TIPODEC',
+    'letra' : 'TIPOLETRA',
+    'LETRA': 'TIPOLETRA',
+    'logico': 'TIPOLOGI',
+    'LOGICO': 'TIPOLOGI',
+    'verdadero': 'TRUE',
+    'VERDADERO': 'TRUE',
+    'falso' : 'FALSE',
+    'FALSO' : 'FALSE',
+    'imprimir': 'ESCRITURA',
+    'IMPRIMIR': 'ESCRITURA',
+    'leer' : 'LECTURA',
+    'LEER' : 'LECTURA',
+    'condicion': 'IF',
+    'CONDICION' : 'IF',
+    'alternativa' : 'ELSE',
+    'ALTERNATIVA': 'ELSE',
+    'para_cada' : 'FOR',
+    'PARA_CADA' : 'FOR',
+    'conforme' : 'CONFORME',
+    'CONFORME' : 'CONFORME',
+    'mientras' : 'WHILE',
+    'MIENTRAS' : 'WHILE',
+    'regresar' : 'RETURN',
+    'REGRESAR' : 'RETURN',
+    'YYY' : 'AND',
+    'OOO' : 'OR',
+    'vacia' : 'VOID',
+    'VACIA': 'VOID',
+}
+
+# Lista de Tokens
+tokens = [
+    'ID',
+    'CTEDECI',
+    'CTEENT',
+    'CTELETRA',
+    'CTEFRASE',
+    'LPAREN',
+    'RPAREN',
+    'LCURLY',
+    'RCURLY',
+    'LBRACE',
+    'RBRACE',
+    'COLON',
+    'COMMA',
+    'ASSIGN',
+    'LT',
+    'GT',
+    'LTE',
+    'GTE',
+    'EQUAL',
+    'NEQUAL',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'COMENTARIO',
+] + list(palabras_reservadas.values())
+
+# Expresiones Regulares - Tokens Sencillos
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LCURLY = r'\{'
+t_RCURLY = r'\}'
+t_LBRACE = r'\['
+t_RBRACE = r'\]'
+t_COLON = r'\:'
+t_COMMA = r'\,'
+t_ASSIGN = r'\='
+t_LT = r'\<'
+t_GT = r'\>'
+t_LTE = r'\<='
+t_GTE = r'\>='
+t_EQUAL = r'\=='
+t_NEQUAL = r'\!='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+
+# Expresiones Regulares - Tokens Complejos
+
+# ID
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*|[\U0001F300-\U0001F5FF\U0001F900-\U0001F9FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF]+'
+    t.type = palabras_reservadas.get(t.value, 'ID')
+    return t
+
+# Expresion regular para decimales
+def t_CTEDECI(t):
+    r'-?\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+# Expresion regular para enteros
+def t_CTEENT(t):
+    r'-?\d+'
+    t.value = int(t.value)
+    return t
+
+# Expresion regular para letras
+def t_CTELETRA(t):
+    r"'[^']'"
+    t.value = str(t.value)
+    return t
+
+# Expresion regular para frases
+def t_CTEFRASE(t):
+    r'"[^"]*"'
+    t.value = str(t.value)
+    return t
+
+# Se ignoran comentarios
+def t_COMENTARIO(t):
+    r'\#{3,}.*'
+    pass
+
+# Se ignoran espacios en blanco
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+
+# Manejo de caracteres invalidos
+def t_error(t):
+    print("Caracter no valido '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Construir el Lexer
+lexer = lex.lex()
---
+++ tests/test_js_lexer.py
@@ -0,0 +1,101 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+from js_lexer import lexer
+
+class MyTestCase(unittest.TestCase):
+
+    # Probar IDs
+    def test_ID(self):
+        results = []
+        input = '''
+            🐍🐍🐍
+            var_hello_world
+            _id_3452
+        '''
+        lexer.input(input)
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            assert tok.type == 'ID'
+
+    # Probar Enteros
+    def test_CTEENT(self):
+        results = []
+        input = '''
+            -3453
+            499392
+            92859
+        '''
+        lexer.input(input)
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            assert tok.type == 'CTEENT'
+
+    # Probar Decimales
+    def test_CTEDECI(self):
+        results = []
+        input = '''
+            -3453.0
+            499392.529482
+            92859.9249
+        '''
+        lexer.input(input)
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            assert tok.type == 'CTEDECI'
+
+    # Probar Letras
+    def test_CTELETRA(self):
+        results = []
+        input = '''
+            'a' '5' '#'
+        '''
+        lexer.input(input)
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            assert tok.type == 'CTELETRA'
+
+    # Probar Frases
+    def test_CTEFRASE(self):
+        results = []
+        input = '''
+            "frase1" "_HelloWorld" "4729302"
+        '''
+        lexer.input(input)
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            assert tok.type == 'CTEFRASE'
+
+
+if __name__ == '__main__':
+    data_input = open("./tests/test_file.txt", "r")
+    input = data_input.read()
+    data_input.close()
+
+    lexer.input(input)
+    tokens = []
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        tokens.append(tok)
+
+    with open("./tests/lexer_output.txt", "x") as output_file:
+        output_file.write("Junior Script Lexer")
+        output_file.write('\n'.join(str(token) for token in tokens))
+
+    unittest.main()
+
+
